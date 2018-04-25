4be37a88afc63da2b6952af66fdb43424e7bd0d0
BeyzaS <beyza.salyador@gmail.com>
2018-04-25 09:08:40 +0200

    Initial commit 2

--- app/src/main/java/com/example/nico/projet/Languages.java
+++ app/src/main/java/com/example/nico/projet/Languages.java
@@ -30,7 +30,7 @@ public class Languages extends AppCompatActivity {
                 break;
         }
         getResources().updateConfiguration(config, getResources().getDisplayMetrics());
-        
+
     }
 
 
